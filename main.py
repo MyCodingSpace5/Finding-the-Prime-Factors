381db0a68304e4ce783200792f63c60d782d761e
Ahmet <121121504+MyCodingSpace5@users.noreply.github.com>
2023-10-29 22:15:51 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+primenumber = input("Enter an prime number")
+primenumberproof = lambda a,b: a/b
+numberofprimefactors = []
+z = 0
+i = 1
+for i in range(100):
+    returnresult = primenumberproof(int(primenumber), i)
+    if(returnresult != int):
+        break
+        print("Not an prime factor")
+        print(returnresult)
+    else:
+        numberofprimefactors.append(returnresult)
+        continue
+for v in int(primenumber) % 2:
+    power = primenumberproof(numberofprimefactors[z], v * 2)
+    if(returnresult != int):
+        break
+        print("Tried to reduce to the simplest form, failed opreation")
+    else:
+        print("Opreation sucessful for" + numberofprimefactors[z])
+        z+=1
