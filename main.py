0a1c2ffb66d195f8ff4bc93fd66928caed160e79
Ahmet <121121504+MyCodingSpace5@users.noreply.github.com>
2023-10-29 22:48:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 primenumber = input("Enter an prime number")
 primenumberproof = lambda a,b: a/b
-numberofprimefactors = []
-z = 0
 i = 1
 for i in range(100):
     returnresult = primenumberproof(int(primenumber), i)
     if(returnresult != int):
-        print("Skipping one prime factor")
+        print("Factor" + "" + i + "Cannot be evenly divided into an prime number")
         continue
     else:
-        numberofprimefactors.append(returnresult)
-        continue
-for v in int(primenumber) % 2:
-    power = primenumberproof(numberofprimefactors[z], int(numberofprimefactors[z]) % 2)
-    if(returnresult != int):
+        print("This number is not an prime number as it has mutiple factors that arent 1")
         break
-        print("Tried to reduce to the simplest form, failed opreation")
-    else:
-        print("Operation successful for" + numberofprimefactors[z])
-        z+=1
+        
